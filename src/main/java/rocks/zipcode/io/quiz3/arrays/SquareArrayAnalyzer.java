1faeaad8bbf129f192543fbd7e08cf8ad3d337c1
Marshilla Brahma <marshillabrahma@zipcoders-MacBook-Pro-10.local>
2019-03-11 09:34:45 -0400

    Completed squareArray and TicTacToe

--- src/main/java/rocks/zipcode/io/quiz3/arrays/SquareArrayAnalyzer.java
+++ src/main/java/rocks/zipcode/io/quiz3/arrays/SquareArrayAnalyzer.java
@@ -1,10 +1,24 @@
 package rocks.zipcode.io.quiz3.arrays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author leon on 09/12/2018.
  */
 public class SquareArrayAnalyzer {
     public static Boolean compare(Integer[] input, Integer[] squaredValues) {
-        return null;
+        List<Integer> list = Arrays.asList(squaredValues);
+        Boolean result = true;
+        for (Integer i : input){
+            if(!list.contains(i*i)){
+                result = false;
+                break;
+            }
+        }
+        return result;
     }
+
+
 }
