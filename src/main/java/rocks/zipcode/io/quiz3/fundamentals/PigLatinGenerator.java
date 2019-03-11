4ee71c0d5b5c2c12f57059a21b999234aa0362cd
Marshilla Brahma <marshillabrahma@zipcoders-MacBook-Pro-10.local>
2019-03-11 13:06:37 -0400

    Completed the pig latin

--- src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
+++ src/main/java/rocks/zipcode/io/quiz3/fundamentals/PigLatinGenerator.java
@@ -8,20 +8,23 @@ public class PigLatinGenerator {
         String[] words = str.split(" ");
         StringBuilder sb = new StringBuilder();
         for(String s : words){
-            if(!VowelUtils.isVowel(s.charAt(0)) && !VowelUtils.isVowel(s.charAt(1))){
+            if(!VowelUtils.isVowel(s.charAt(0)) && !VowelUtils.isVowel(s.charAt(1)) && VowelUtils.isVowel(s.charAt(2))){
                 sb.append(s.substring(2) + s.substring(0,2) + "ay");
                 sb.append(" ");
-            }else{
+            }else if(!VowelUtils.hasVowels(s)){
+                sb.append(s + "ay");
+                sb.append(" ");
+            } else{
                 if(VowelUtils.startsWithVowel(s)){
                     sb.append(s+"way");
                     sb.append(" ");
                 }
                 if(!VowelUtils.startsWithVowel(s)){
-                    sb.append(s.substring(1)+s.substring(0,1)+"ay");
+                    int i = VowelUtils.getIndexOfFirstVowel(s);
+                    sb.append(s.substring(i)+s.substring(0,i)+"ay");
                     sb.append(" ");
                 }
             }
-
         }
         sb.setLength(sb.length() - 1);
         return sb.toString();
